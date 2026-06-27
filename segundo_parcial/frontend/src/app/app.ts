9e037f7f17d77f094b16172d76458db7a82b336b
Silvina Gauto <silvigauto02@gmail.com>
2026-06-27 20:32:44 -0300

    estructura correcta frontend y backend

--- segundo_parcial/frontend/src/app/app.ts
+++ segundo_parcial/frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('segundo_parcial');
+  protected readonly title = signal('frontend');
 }
