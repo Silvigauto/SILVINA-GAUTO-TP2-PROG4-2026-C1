cc1cdc29b46ad4d81039d18c4ca0d58cb29df57b
Silvina Gauto <silvigauto02@gmail.com>
2026-06-28 01:37:20 -0300

    enviroment actualizado

--- segundo_parcial/frontend/src/environments/environment.ts
+++ segundo_parcial/frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'silvina-gauto-tp-2-prog-4-2026-c1-f.vercel.app'
+  apiUrl: 'https://silvina-gauto-tp-2-prog-4-2026-c1-f.vercel.app'
 
 };
