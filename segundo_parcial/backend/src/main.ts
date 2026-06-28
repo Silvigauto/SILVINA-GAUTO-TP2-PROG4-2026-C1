b2c21be25049a2fffcb47036d63cffe5f0185f26
Silvina Gauto <silvigauto02@gmail.com>
2026-06-28 01:08:55 -0300

    deploy backend

--- segundo_parcial/backend/src/main.ts
+++ segundo_parcial/backend/src/main.ts
@@ -3,10 +3,14 @@ dns.setServers(['8.8.8.8', '1.1.1.1']); // NO SACAR
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); //para que pueda correr desde dos puertos distintos y no de error
+  
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+  
+  app.enableCors({origin '*'}); //para que pueda correr desde dos puertos distintos y no de error
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
